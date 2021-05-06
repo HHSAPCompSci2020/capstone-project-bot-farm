48001da5601b53fee3b9883edcc0ce40384054fe
Michael Chen <mchen200@student.fuhsd.org>
2021-05-06 13:22:35 -0700

    fixed main method

--- Bot Farm Capstone/src/Main.java
+++ Bot Farm Capstone/src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 
 public class Main { 
     
-	public static void main(String args[]) {
+	public static void main(String[] args) {
       	DrawingSurface drawing = new DrawingSurface();
 	}
      
