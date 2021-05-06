e563bb750707df96e74a2c70828f0d35c837495b
huaicodes <huaicodes@users.noreply.github.com>
2021-05-06 13:26:12 -0700

    window

--- Bot Farm Capstone/src/Main.java
+++ Bot Farm Capstone/src/Main.java
@@ -5,6 +5,12 @@ public class Main {
     
 	public static void main(String[] args) {
       	DrawingSurface drawing = new DrawingSurface();
+      	JFrame window = new JFrame();
+		window.setSize(200,200);
+		window.setMinimumSize(new Dimension(100,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.requestFocus();
+		window.setVisible(true);
 	}
      
 }
