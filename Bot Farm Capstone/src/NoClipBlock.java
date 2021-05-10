bc28f52231a7336c80e450f0cae7a3c9d9a1d385
huaicodes <huaicodes@users.noreply.github.com>
2021-05-10 13:24:41 -0700

    Update NoClipBlock.java

--- Bot Farm Capstone/src/NoClipBlock.java
+++ Bot Farm Capstone/src/NoClipBlock.java
@@ -1,4 +1,7 @@
+import processing.core.PImage;
 
 public class NoClipBlock extends Block {
-
+	public NoClipBlock(PImage image, int x, int y, int width, int height) {
+        super(image,x,y,width,height);
+    }
 }
