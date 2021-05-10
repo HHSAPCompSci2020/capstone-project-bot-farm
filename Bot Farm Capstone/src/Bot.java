2f0ca762fe6164fc65bcbee1bf04eadb0357b31e
Michael Chen <mchen200@student.fuhsd.org>
2021-05-10 13:22:30 -0700

    created bot class

--- Bot Farm Capstone/src/Bot.java
+++ Bot Farm Capstone/src/Bot.java
@@ -1,4 +1,51 @@
 
-public class Bot {
+import processing.core.PImage;
 
+public abstract class Bot extends MovingImage {
+    
+    protected int ehp, vX, vY; //enemy hp, velocity x and velocity y
+    protected final int OEHP; //max hp
+    protected boolean edead; //true or false dead
+    
+    public Bot(PImage image,int x, int y, int width, int height, int hp){
+            super(image, x, y, width, height);
+            this.OEHP = hp;
+            this.ehp = OEHP;
+            this.vX = 0;
+            this.vY = 0;
+            this.edead = false; //not dead :(
+    }
+    
+    public void setvX(int x){
+        vX = x;
+    }
+
+    public void setvY(int y) {
+        vY = y;
+    }
+    
+    public void loseHP(){
+        ehp -= 5;
+        if(ehp <= 0){
+            die(); //dies when hp less than zero
+            sound("oof.wav");
+        }
+    }
+
+    public static void sound(String filepath) {
+       
+    }
+
+    
+        
+    private void die(){
+        edead = true; //Sets the dead boolean to true
+    }
+
+    public boolean isDead(){
+        return edead; //Change the below statement to return an accurate value.
+    }
+    
+    public abstract Projectile shoot(int x, int y);
+    
 }
