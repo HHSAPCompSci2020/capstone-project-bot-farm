29ac59bbc500546a0a213e4f12f611285c77cb1c
zhe730 <zackeryhe@zackerys-mbp.lan>
2021-05-10 13:53:20 -0700

    Update ExploBot.java

--- Bot Farm Capstone/src/ExploBot.java
+++ Bot Farm Capstone/src/ExploBot.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
 
-public class ExploBot {
+import processing.core.PApplet;
+import processing.core.PImage;
 
+public class ExploBot extends Bot {
+	protected int counter;
+	protected boolean dead;
+	public ExploBot(PImage image, int x, int y, int width, int height) {
+		super(image, x,y,width,height,50);
+		
+	}
+	
+	public void draw(PApplet marker) {
+		super.draw(marker);
+	}
+	
+	public MovingImage act(ArrayList<MovingImage> list){
+		return null;
+	}
+
+	@Override
+	public Projectile shoot(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String toString() {
+		return "bot";
+	}
 }
