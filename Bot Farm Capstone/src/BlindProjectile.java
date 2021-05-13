140c745779559cad61c648c69c45cbad5b095c0a
Michael Chen <mchen200@student.fuhsd.org>
2021-05-12 18:28:41 -0700

    prototype act for blindbot

--- Bot Farm Capstone/src/BlindProjectile.java
+++ Bot Farm Capstone/src/BlindProjectile.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import processing.core.PImage;
 
 public class BlindProjectile extends Projectile {
@@ -5,5 +7,4 @@ public class BlindProjectile extends Projectile {
 	public BlindProjectile(PImage img, double x, double y, int w, int h, String owner, double angle, int delay) {
 		super(img, x, y, w, h, owner, angle, delay);
 	}
-	
 }
