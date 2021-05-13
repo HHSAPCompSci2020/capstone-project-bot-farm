b4fe64fe3222f40012552e210f8611fd7c97ca99
huaicodes <huaicodes@users.noreply.github.com>
2021-05-13 10:35:55 -0700

    int double bug

--- Bot Farm Capstone/src/AndroidBasicProjectile.java
+++ Bot Farm Capstone/src/AndroidBasicProjectile.java
@@ -2,4 +2,9 @@ import processing.core.PImage;
 
 public class AndroidBasicProjectile extends Projectile{
 
+	public AndroidBasicProjectile(PImage img, int x, int y, int w, int h, String owner, double angle, int delay) {
+		super(img, x, y, w, h, owner, angle, delay);
+		// TODO Auto-generated constructor stub
+	}
+
 }
