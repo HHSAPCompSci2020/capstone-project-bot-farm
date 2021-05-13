82686d820baa8ee5744dbc046465e1033841f5b5
huaicodes <huaicodes@users.noreply.github.com>
2021-05-13 11:24:53 -0700

    double angle

--- Bot Farm Capstone/src/ExploBotBabyProjectile.java
+++ Bot Farm Capstone/src/ExploBotBabyProjectile.java
@@ -2,8 +2,8 @@ import processing.core.PImage;
 
 public class ExploBotBabyProjectile extends Projectile {
 
-	public ExploBotBabyProjectile(PImage img, int x, int y, int w, int h, String owner, int delay) {
-		super(img, x, y, w, h, owner, delay);
+	public ExploBotBabyProjectile(PImage img, int x, int y, int w, int h, String owner, double angle, int delay) {
+		super(img, x, y, w, h, owner, angle, delay);
 		// TODO Auto-generated constructor stub
 	}
 
