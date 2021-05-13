bed5da766a65be1e2a13f3a3d1461956d29cd18c
zhe730 <zackeryhe@zackerys-mbp.lan>
2021-05-13 11:23:18 -0700

    explobot path

--- Bot Farm Capstone/src/ExploBotBaby.java
+++ Bot Farm Capstone/src/ExploBotBaby.java
@@ -3,18 +3,53 @@ import java.util.ArrayList;
 import processing.core.PImage;
 
 public class ExploBotBaby extends Bot{
+	private final int SPEED = 10;
 	public ExploBotBaby(PImage image, PImage i2, double x, double y, int width, int height) {
 		super(image, x,y,width,height,50);
 		
 	}
+	//taken from blind bot
 	public MovingImage act(ArrayList<MovingImage> list){
-		return super.act(list);
+		if (counter%100 == 0){
+			Player p = null;
+			for (MovingImage m : list) {
+				if (m instanceof Player) {
+					p = (Player) m;
+				}
+			}
+			int pX = (int) p.getX();
+			int pY = (int) p.getY();
+			list.add(this.shoot(pX, pY));
+		} else if(counter%20 == 0){
+			Player p = null;
+			for (MovingImage m : list) {
+				if (m instanceof Player) {
+					p = (Player) m;
+				}
+			}
+			double angle = Math.tan((p.getY() - this.y) / (p.getX() - this.x));
+
+			vY = SPEED*(int)Math.sin(angle);
+			vX = SPEED*(int)Math.cos(angle);
+		}
+		else {
+			vY /= 2;
+			vX /= 2;
+		}
+		counter++; //Adds to counter
+		if(!this.isInWindow() || this.isDead()){
+			return this;
+		}    
+		return null;
+
 	}
 	@Override
 	public Projectile shoot(int x, int y) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	private 
 	
 	public void die() {
 		shoot((int)getX(),(int)getY());
