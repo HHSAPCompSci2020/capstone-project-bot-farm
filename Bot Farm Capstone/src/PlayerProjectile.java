dd676b4b6c3d5c17894118060ba80ea173d1948b
huaicodes <huaicodes@users.noreply.github.com>
2021-05-13 12:31:29 -0700

    deleted playerprojectile

--- Bot Farm Capstone/src/PlayerProjectile.java
+++
@@ -1,7 +0,0 @@
-import processing.core.*;
-
-public class PlayerProjectile extends Projectile {
-	public PlayerProjectile(PImage img, int x, int y, int w, int h, String owner, double angle, int delay) {
-        super(img, x, y, w, h, owner, angle, delay);           
-	}
-}
