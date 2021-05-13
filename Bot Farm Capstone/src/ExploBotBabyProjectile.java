def89e448a124080f9a9a8fc718c2077f0a84163
zhe730 <zackeryhe@zackerys-mbp.lan>
2021-05-12 18:50:35 -0700

    Update ExploBotBabyProjectile.java

--- Bot Farm Capstone/src/ExploBotBabyProjectile.java
+++ Bot Farm Capstone/src/ExploBotBabyProjectile.java
@@ -2,4 +2,9 @@ import processing.core.PImage;
 
 public class ExploBotBabyProjectile extends Projectile {
 
+	public ExploBotBabyProjectile(PImage img, int x, int y, int w, int h, String owner, int delay) {
+		super(img, x, y, w, h, owner, delay);
+		// TODO Auto-generated constructor stub
+	}
+
 }
