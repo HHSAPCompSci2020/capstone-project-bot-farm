8f8e4f73ff1a820ae51c4c5b14805cf2fd81959a
Michael Chen <mchen200@student.fuhsd.org>
2021-05-12 17:52:05 -0700

    created blind bot and blind projectile

--- Bot Farm Capstone/src/BlindBot.java
+++ Bot Farm Capstone/src/BlindBot.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
 
-public class BlindBot {
+import processing.core.PImage;
+
+public class BlindBot extends Bot {
+	private final double DMG_RADIUS = 5;
+	private final int AOE_DMG = 5;
+	public BlindBot(PImage image, int x, int y, int width, int height, int hp) {
+		super(image, x, y, width, height, hp);
+	}
+
+	public void damage(Player player) {
+		double distance = Math.sqrt(Math.pow(player.getY() - this.y, 2) + Math.pow(player.getX() - this.x, 2));
+		if (distance <= DMG_RADIUS)
+			for (int i = 0; i < AOE_DMG; i++)
+				player.loseHP();
+	}
+	public MovingImage act(ArrayList<MovingImage> list) {
+		// pathfinding work in progress
+		return super.act(list);
+	}
+	public Projectile shoot(int x, int y) {
+		BlindProjectile proj = new BlindProjectile(image, this.x, this.y, width, height, 
+				"blindbot", Math.tan((double)y / (double)x), 0);
+		return proj;
+	}
 
 }
