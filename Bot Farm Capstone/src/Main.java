c8d09fc388a613350b5a891bbfa75bf66e54f356
Zackery He <69951963+zhe730@users.noreply.github.com>
2021-05-22 21:58:28 -0700

    audio

--- Bot Farm Capstone/src/Main.java
+++ Bot Farm Capstone/src/Main.java
@@ -1,27 +1,58 @@
 import java.awt.Dimension;
+import java.io.File;
+import java.io.IOException;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 
 import processing.awt.PSurfaceAWT;
 import processing.core.PApplet;
 
-public class Main { 
-    
+public class Main {
+
 	public static void main(String[] args) {
 		DrawingSurface drawing = new DrawingSurface();
-		PApplet.runSketch(new String[]{""}, drawing);
+		PApplet.runSketch(new String[] { "" }, drawing);
 		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
 		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
-		JFrame window = (JFrame)canvas.getFrame();
+		JFrame window = (JFrame) canvas.getFrame();
 
 		window.setBounds(0, 0, 750, 750);
-		window.setMinimumSize(new Dimension(100,100));
+		window.setMinimumSize(new Dimension(100, 100));
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 
 		window.setVisible(true);
-		
+
 		canvas.requestFocus();
+		
+		//edited from https://www.geeksforgeeks.org/play-audio-file-using-java/
+
+		String filePath = "gamemusic.wav";
+		AudioInputStream audioInputStream;
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+			// create clip reference
+			Clip clip = AudioSystem.getClip();
+
+			// open audioInputStream to the clip
+			clip.open(audioInputStream);
+			FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+			double gain = 0.1;   
+			float dB = (float) (Math.log(gain) / Math.log(10.0) * 20.0);
+			gainControl.setValue(dB);
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
 	}
-     
+
 }
